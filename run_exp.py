65af462d07e61d21717c662ee72114e6c979d010
myazann <mertyazan2867@gmail.com>
2024-09-16 16:53:09 +0200

    removed summary, added ce

--- run_exp.py
+++ run_exp.py
@@ -7,7 +7,7 @@ import subprocess
 
 from models import LLM
 from utils import get_args, get_k
-from prompts import prepare_res_prompt, prepare_summary_prompt
+from prompts import prepare_res_prompt
 from exp_datasets import LampDataset, AmazonDataset
 from feature_processor import FeatureProcessor
 from retriever import Retriever
@@ -29,13 +29,14 @@ else:
 gts = dataset.get_gt()
 retriever = Retriever(dataset, args.retriever)
 
-MAX_NEW_TOKENS = 64
+MAX_NEW_TOKENS = 64 if dataset.name == "lamp" else 128
 pred_path = "preds"
 os.makedirs(pred_path, exist_ok=True)
 if dataset_name == "lamp":
     ids = dataset.get_ids()    
 
-LLMs = ["GEMMA-2-9B", "GEMMA-2-27B"]
+# LLMs = ["GPT-4o-mini", "GEMMA-2-9B", "GEMMA-2-27B"]
+LLMs = ["GEMMA-2-9B"]
 queries, retr_texts, retr_gts = dataset.get_retr_data() 
 if not args.k:
     k = get_k(retr_texts)
@@ -45,12 +46,18 @@ else:
 all_context = retriever.get_context(queries, retr_texts, retr_gts, k) 
 if args.features:
     feature_processor = FeatureProcessor()
-    all_features = feature_processor.get_features(dataset.tag, args.features, retr_texts, retr_gts)
+    all_features = feature_processor.get_all_features(args.dataset, args.features, retr_texts, retr_gts)
+    prepared_features = feature_processor.prepare_features(all_features, args.features)
+else:
+    features = None
 
-print(f"Running experiments for {args.dataset} with Features: {args.features}, Retriever: {args.retriever}, Summary: {args.summary}, and K: {k}")
+if args.counter_examples:
+    _, query_retr_res = retriever.get_retrieval_results(queries, queries)
+
+print(f"Running experiments for {args.dataset} with Features: {args.features}, Retriever: {args.retriever}, and K: {k}")
 sys.stdout.flush()
 for model_name in LLMs:
-    exp_name = f"{args.dataset}_{model_name}_{args.features}_{args.retriever}_K({k})_Sum({args.summary})"
+    exp_name = f"{args.dataset}_{model_name}_{args.features}_{args.retriever}_K({k}))"
     pred_out_path = f"{pred_path}/{exp_name}.json"
     if os.path.exists(pred_out_path):
         with open(pred_out_path, "rb") as f:
@@ -65,43 +72,41 @@ for model_name in LLMs:
 
     llm = LLM(model_name=model_name, gen_params={"max_new_tokens": MAX_NEW_TOKENS})
 
-    queries = queries[len(all_res):]
-    retr_texts = retr_texts[len(all_res):]
-    retr_gts = retr_gts[len(all_res):]
-    gts = gts[len(all_res):]
-    all_context = all_context[len(all_res):]
-    if args.features:
-        all_features = all_features[len(all_res):]
-    else:
-        features = None
-
     print(f"Starting from sample no. {len(all_res)}")
     start_time = time.time()
     print(subprocess.run("gpustat")) 
     sys.stdout.flush() 
 
-    for i in range(len(queries)):
+    for i in range(len(queries) - len(all_res)):
 
-        query = queries[i]        
-        context = all_context[i]            
+        cont_idx = len(all_res) + i
+        query = queries[cont_idx]        
+        context = all_context[cont_idx]            
         if args.features:
-            features = all_features[i]
+            features = prepared_features[cont_idx]
+        
+        if args.counter_examples:
+            _, retr_gt_name, retr_prompt_name = dataset.get_var_names()
+            ce_idxs = query_retr_res[cont_idx][-3:]
+            ce_examples = ""
+            for ce_i, ce in enumerate(ce_idxs):
+                ce_example = ""
+                for j in range(3):
+                    retr_texts[ce][j]
+                    ce_example = f"{ce_example}\n{retr_prompt_name.capitalize()}:\n{retr_texts[ce][j]}\n{retr_gt_name.capitalize()}:\n{retr_gts[ce][j]}"
+                ce_examples = f"{ce_examples}\n<Other Writer-{ce_i+1}>{ce_example}\n</Other Writer-{ce_i+1}>"
+
+        print(ce_examples)
 
         start_bot_time = time.time() 
-        if args.summary:
-            sum_prompt = prepare_summary_prompt(dataset, llm, context, features)
-            sum_prompt = [{"role": "user", "content": sum_prompt}]
-            res = llm.prompt_chatbot(sum_prompt, gen_params={"max_new_tokens": 256})
-            features = [res]
-
         prompt = prepare_res_prompt(dataset, query, llm, examples=context, features=features)
         prompt = [{"role": "user", "content": prompt}]
            
         res = llm.prompt_chatbot(prompt)
         # print(f"Pred: {res}")
 
-        id = ids[i] if dataset_name == "lamp" else i
-        # print(f"GT: {gts[i]}")
+        id = ids[cont_idx] if dataset_name == "lamp" else i
+        # print(f"GT: {gts[cont_idx]}")
         end_bot_time = time.time()
         all_res.append({
                 "id": id,
@@ -110,8 +115,8 @@ for model_name in LLMs:
                 "model_inf_time": round(end_bot_time - start_bot_time, 2), 
         })
 
-        if (i+1)%500==0 or (i+1)==len(queries):
-            print(i)
+        if (cont_idx+1)%500==0 or (cont_idx+1)==len(queries):
+            print(cont_idx)
             with open(pred_out_path, "w") as f:
                 task = f"LaMP_{num}" if dataset_name == "lamp" else args.dataset          
                 json.dump({
