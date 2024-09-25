1c9cccc38578b91d8ddfdde594413dbc5d1e7fcb
Mert Yazan <myazan1@int5.local.snellius.surf.nl>
2024-09-25 10:38:01 +0200

    test set results

--- prep_lamp_res.py
+++ prep_lamp_res.py
@@ -3,13 +3,15 @@ import json
 
 for file in os.listdir("preds"):
 
-    with open(os.path.join("preds", file), "r") as f:
-        res = json.load(f)
-    
-    keys_to_keep = ["id", "output"]
-    filtered_list = [{k: d[k] for k in keys_to_keep if k in d} for d in res["golds"]]
+    if file.startswith("lamp") and file.split("_")[2] == "test":
+        
+        with open(os.path.join("preds", file), "r") as f:
+            res = json.load(f)
+        
+        keys_to_keep = ["id", "output"]
+        filtered_list = [{k: d[k] for k in keys_to_keep if k in d} for d in res["golds"]]
 
-    res["golds"] = filtered_list
-    os.makedirs("lamp_preds", exist_ok=True)
-    with open(os.path.join("lamp_preds", file), "w") as f:
-        json.dump(res, f)
+        res["golds"] = filtered_list
+        os.makedirs("lamp_preds", exist_ok=True)
+        with open(os.path.join("lamp_preds", file), "w") as f:
+            json.dump(res, f)
